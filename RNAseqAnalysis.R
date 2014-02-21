bd8c01492f24cef5eb8f997394a827945db87163
Jyota <jyotas@my.ccsu.edu>
2014-02-20 21:36:46 -0800

    add more plots for variance related stuff with RNAseq transform

--- RNAseqAnalysis.R
+++ RNAseqAnalysis.R
@@ -42,6 +42,35 @@ maxVarPlot$Class <- 'Tumor'
 maxVarPlot[maxVarPlot$class==0,]$Class <- 'Healthy'
 ggplot(maxVarPlot,aes(value,fill=Class))+geom_density(alpha=.8)+theme_bw()+scale_x_continuous(labels=comma)+facet_wrap(~ gene)+theme(axis.text.x=element_text(angle=90))+labs(fill='Class')+xlab('')
 
+# Check out minimum variance genes for non-informative set pre & post transform..
+nonInfVars <- cmpVars[cmpVars$setOfVars=='Noninformative Set',]
+nonInfVars <- nonInfVars[order(nonInfVars$variance,decreasing=F),]
+niGenes <- rownames(nonInfVars)[1:4]
+maxVarPlot <- data.frame(gene=c(rep(niGenes[1],length(classes)),rep(niGenes[2],length(classes)),
+	rep(niGenes[3],length(classes)),rep(niGenes[4],length(classes))),
+    class=c(rep(classes,4)),value=c(castTrainingSet[,which(colnames(castTrainingSet) %in% niGenes[1])],
+    	castTrainingSet[,which(colnames(castTrainingSet) %in% niGenes[2])],castTrainingSet[,which(colnames(castTrainingSet) %in% niGenes[3])],
+    	castTrainingSet[,which(colnames(castTrainingSet) %in% niGenes[4])]))
+
+maxVarPlot$Class <- 'Tumor'
+maxVarPlot[maxVarPlot$class==0,]$Class <- 'Healthy'
+ggplot(maxVarPlot,aes(value,fill=Class))+geom_density(alpha=.8)+theme_bw()+scale_x_continuous(labels=comma)+facet_wrap(~ gene)+theme(axis.text.x=element_text(angle=90))+labs(fill='Class')+xlab('')
+
+# Check out post transform plot of these
+nonInfVars <- cmpVars[cmpVars$setOfVars=='Noninformative Set',]
+nonInfVars <- nonInfVars[order(nonInfVars$variance,decreasing=F),]
+niGenes <- rownames(nonInfVars)[1:4]
+maxVarPlot <- data.frame(gene=c(rep(niGenes[1],length(classes)),rep(niGenes[2],length(classes)),
+	rep(niGenes[3],length(classes)),rep(niGenes[4],length(classes))),
+    class=c(rep(classes,4)),value=c(readyTrainingSet[,which(colnames(readyTrainingSet) %in% niGenes[1])],
+    	readyTrainingSet[,which(colnames(readyTrainingSet) %in% niGenes[2])],readyTrainingSet[,which(colnames(readyTrainingSet) %in% niGenes[3])],
+    	readyTrainingSet[,which(colnames(readyTrainingSet) %in% niGenes[4])]))
+
+maxVarPlot$Class <- 'Tumor'
+maxVarPlot[maxVarPlot$class==0,]$Class <- 'Healthy'
+ggplot(maxVarPlot,aes(value,fill=Class))+geom_density(alpha=.8)+theme_bw()+scale_x_continuous(labels=comma)+facet_wrap(~ gene)+theme(axis.text.x=element_text(angle=90))+labs(fill='Class')+xlab('')
+
+
 # Now do the same for the informative set, except check smallest variances to see if 'inflation' happened.
 InfVars <- cmpVars[cmpVars$setOfVars=='Informative Set',]
 InfVars <- InfVars[order(InfVars$variance,decreasing=F),]
@@ -58,6 +87,33 @@ ggplot(maxVarPlot,aes(value,fill=Class))+geom_density(alpha=.8)+theme_bw()+scale
 
 InfVars <- cmpVars[cmpVars$setOfVars=='Informative Set',]
 InfVars <- InfVars[order(InfVars$variance,decreasing=F),]
+niGenes <- rownames(InfVars)[1:4]
+maxVarPlot <- data.frame(gene=c(rep(niGenes[1],length(classes)),rep(niGenes[2],length(classes)),
+	rep(niGenes[3],length(classes)),rep(niGenes[4],length(classes))),
+    class=c(rep(classes,4)),value=c(readyTrainingSet[,which(colnames(readyTrainingSet) %in% niGenes[1])],
+    	readyTrainingSet[,which(colnames(readyTrainingSet) %in% niGenes[2])],readyTrainingSet[,which(colnames(readyTrainingSet) %in% niGenes[3])],
+    	readyTrainingSet[,which(colnames(readyTrainingSet) %in% niGenes[4])]))
+
+maxVarPlot$Class <- 'Tumor'
+maxVarPlot[maxVarPlot$class==0,]$Class <- 'Healthy'
+ggplot(maxVarPlot,aes(value,fill=Class))+geom_density(alpha=.8)+theme_bw()+scale_x_continuous(labels=comma)+facet_wrap(~ gene)+theme(axis.text.x=element_text(angle=90))+labs(fill='Class')+xlab('')
+
+# Check out max variance genes of informative set pre & post transform
+InfVars <- cmpVars[cmpVars$setOfVars=='Informative Set',]
+InfVars <- InfVars[order(InfVars$variance,decreasing=T),]
+niGenes <- rownames(InfVars)[1:4]
+maxVarPlot <- data.frame(gene=c(rep(niGenes[1],length(classes)),rep(niGenes[2],length(classes)),
+	rep(niGenes[3],length(classes)),rep(niGenes[4],length(classes))),
+    class=c(rep(classes,4)),value=c(castTrainingSet[,which(colnames(castTrainingSet) %in% niGenes[1])],
+    	castTrainingSet[,which(colnames(castTrainingSet) %in% niGenes[2])],castTrainingSet[,which(colnames(castTrainingSet) %in% niGenes[3])],
+    	castTrainingSet[,which(colnames(castTrainingSet) %in% niGenes[4])]))
+
+maxVarPlot$Class <- 'Tumor'
+maxVarPlot[maxVarPlot$class==0,]$Class <- 'Healthy'
+ggplot(maxVarPlot,aes(value,fill=Class))+geom_density(alpha=.8)+theme_bw()+scale_x_continuous(labels=comma)+facet_wrap(~ gene)+theme(axis.text.x=element_text(angle=90))+labs(fill='Class')+xlab('')
+
+InfVars <- cmpVars[cmpVars$setOfVars=='Informative Set',]
+InfVars <- InfVars[order(InfVars$variance,decreasing=T),]
 niGenes <- rownames(InfVars)[1:4]
 maxVarPlot <- data.frame(gene=c(rep(niGenes[1],length(classes)),rep(niGenes[2],length(classes)),
 	rep(niGenes[3],length(classes)),rep(niGenes[4],length(classes))),
