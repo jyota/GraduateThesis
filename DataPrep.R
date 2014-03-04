f4ed22445d2106947c98321d2253179feb9eda2d
Jyota <jyotas@my.ccsu.edu>
2014-03-03 22:31:12 -0800

    update data prep filtering criteria

--- DataPrep.R
+++ DataPrep.R
@@ -4,7 +4,7 @@ require(reshape)
 require(scales)
 require(matrixStats)
 require(grid)
-require(DESeq2)
+require(edgeR)
 setwd("~/data/RNASeqV2/UNC__IlluminaHiSeq_RNASeqV2/Level_3/")
 # Load all the TCGA files into one matrixx and transform it for use.
 listFiles = list.files(pattern="*rsem_gene.txt")
@@ -24,26 +24,24 @@ write.table(castTrainingSet,"mungedTrainingSet.txt",sep='\t',col.names=T,row.nam
 # Now can just read from this following statement... instead of performing the above.
 castTrainingSet <- read.table("~/Thesis/mungedTrainingSet.txt",sep='\t',header=T,check.names=F,row.names=1)
 
-# Revising the data preparation... with Upper quartile normalization then log2 transform. Filtering those with < 50 
+# Revising the data preparation... with Upper quartile normalization then log2 transform. Filtering those with <= 50 cpm after scaling.
 classes <- castTrainingSet$class 
 castTrainingSet$class <- NULL
 source('~/Thesis/upperQuartileNormalize.R')
 vTraining <- t(upperQuartileNormalize(t(castTrainingSet)))
-checkGenes <- data.frame(genes=colnames(vTraining),tumorPresentPct=rep(0,length(colnames(vTraining))),healthyPresentPct=rep(0,length(colnames(vTraining))))
-calculateGenes <- data.frame(vTraining>50,check.names=F)
-calculateGenes$class <- classes
+checkGenes <- data.frame(genes=colnames(vTraining),tumorPresent=rep(0,length(colnames(vTraining))),healthyPresent=rep(0,length(colnames(vTraining))))
+checkGenes$genes <- as.character(checkGenes$genes)
+calculateGenes <- data.frame(cpm(t(vTraining))>25,check.names=F)
+calculateGenes <- t(calculateGenes)
+calculateGenes <- data.frame(calculateGenes,class=classes,check.names=F)
 for(i in 1:NROW(checkGenes)){
 	checkGenes[i,1] <- colnames(calculateGenes)[i]
 	checkGenes[i,2] <- sum(calculateGenes[calculateGenes$class==1,i])/length(classes[classes==1])
 	checkGenes[i,3] <- sum(calculateGenes[calculateGenes$class==0,i])/length(classes[classes==0])
 }
-genesToRemove <- checkGenes[checkGenes$tumorPresentPct < .25 & checkGenes$healthyPresentPct < .25,]$genes
+genesToRemove <- checkGenes[checkGenes$tumorPresent < .5 & checkGenes$healthyPresent < .5,]$genes
 interTrainingSet <- vTraining[,-which(colnames(vTraining) %in% genesToRemove)]
 readyTrainingSet <- log2(interTrainingSet+1.0)
-
-# Check out DESeq2's rlog transform.. not sure how to describe it so probably won't actually use for this thesis anyway.
-q = DESeqDataSetFromMatrix(countData=as.matrix(t(checkTrainingSet)),colData=data.frame(class=classes),design=~ class)
-rlogq = rlogTransformation(q,blind=T)
 
 # Select some random genes for before/after plots.
 set.seed(323)
@@ -57,13 +55,13 @@ ggplot(postGeneExamine,aes(value)) + geom_histogram() + facet_wrap(~ variable) +
 
 # Boxplots - randomly choose samples.
 randSampleNames = sample(rownames(readyTrainingSet),9)
-# Just look at those after filtering... but before/after transformation with limma voom.
+# Just look at those after filtering... but before/after transformat.
 preGeneExamine = melt(data.frame(barcode=rownames(castTrainingSet[rownames(castTrainingSet) %in% randSampleNames,which(colnames(castTrainingSet) %in% colnames(readyTrainingSet))]),castTrainingSet[rownames(castTrainingSet) %in% randSampleNames,which(colnames(castTrainingSet) %in% colnames(readyTrainingSet))],check.names=F))
 ggplot(preGeneExamine,aes(factor(barcode),value,fill=factor(barcode))) + geom_boxplot() + ylab("Expected Count of Short Reads Derived from Gene") + xlab("Sample") + theme_bw() + theme(plot.margin=unit(c(1,1,1,1),'lines'),axis.text.x = element_text(angle=90,hjust=1),legend.position='none') + scale_y_continuous(labels=comma) 
 postGeneExamine = melt(data.frame(barcode=rownames(readyTrainingSet[rownames(readyTrainingSet) %in% randSampleNames,]),readyTrainingSet[rownames(readyTrainingSet) %in% randSampleNames,],check.names=F))
 ggplot(postGeneExamine,aes(factor(barcode),value,fill=factor(barcode))) + stat_boxplot(geom='errorbar') + geom_boxplot() + ylab("Expected Count of Short Reads Derived from Gene (transformed)") + xlab("Sample") + theme_bw() + theme(plot.margin=unit(c(1,1,1,1),'lines'),axis.text.x = element_text(angle=90,hjust=1),legend.position='none') + scale_y_continuous(labels=comma) 
 
-# Mean vs. Variance plots (before and after limma voom, excluding genes filtered out for all plots)
+# Mean vs. Variance plots (before and after upper quartile normalization excluding genes filtered out for all plots)
 options(scipen=999)
 prefiltMeanVar = data.frame(Mean=colMeans(castTrainingSet[,which(colnames(castTrainingSet) %in% colnames(readyTrainingSet))]),Variance=colVars(castTrainingSet[,which(colnames(castTrainingSet) %in% colnames(readyTrainingSet))]))
 ggplot(prefiltMeanVar,aes(x=Mean,y=Variance)) + geom_point() + scale_y_continuous(labels=comma) + scale_x_continuous(labels=comma) + theme(axis.title.x=element_text(vjust=-.5),axis.title.y=element_text(vjust=-.05), plot.margin=unit(c(1,1,1,1),'lines')) + theme_bw()
@@ -76,7 +74,6 @@ prefiltMeanVar <- NULL
 postfilMeanVar <- NULL
 castTrainingSet <- NULL
 trainingSet <- NULL
-
 
 # Interact with NCBI's SRA db to get metadata for independent test data set.
 require(SRAdb)
