cf13069545ef5bc8a1d78f19149d6f16ff1a9eaa
Jyota <jyotas@my.ccsu.edu>
2014-03-15 14:52:27 -0700

    updated biomarker discovery script

--- BiomarkerDiscovery.R
+++ BiomarkerDiscovery.R
@@ -1,6 +1,7 @@
 # Getting into the feature selection steps.
 setwd("~/data/RNASeqV2/UNC__IlluminaHiSeq_RNASeqV2/Level_3/FeatureSelection/")
 source('findInformativeBagging.R')
+source('modifiedBagging.R')
 
 # Generate results for biomarkers of size p = 2 through 10. With 80% proportion in training set, 20% in out of bag validation.
 P10Result = findInformativeBagging(as.matrix(readyTrainingSet),classes,rep=100,stopP=10,stopT2=1000,proportion=.8)
@@ -23,14 +24,41 @@ P2Result = findInformativeBagging(as.matrix(readyTrainingSet),classes,rep=100,st
 write.table(P2Result,"~/Thesis/p2result.txt",sep='\t',col.names=T,row.names=T)
 
 # Plots of accuracy, sensitivity, and specificity for various P
-repAcc = data.frame(accuracy=c(P2Result$repStats[,1],P3Result$repStats[,1],P4Result$repStats[,1],P5Result$repStats[,1],P6Result$repStats[,1],P7Result$repStats[,1],P8Result$repStats[,1],P9Result$repStats[,1],P10Result$repStats[,1]),P=c(rep(2,100),rep(3,100),rep(4,100),rep(5,100),rep(6,100),rep(7,100),rep(8,100),rep(9,100),rep(10,100)))
-repAcc$P = as.factor(repAcc$P)
+repAcc <- data.frame(accuracy=c(as.numeric(as.character(P2Result$Accuracy)),
+	as.numeric(as.character(P3Result$Accuracy)),
+	as.numeric(as.character(P4Result$Accuracy)),
+	as.numeric(as.character(P5Result$Accuracy)),
+	as.numeric(as.character(P6Result$Accuracy)),
+	as.numeric(as.character(P7Result$Accuracy)),
+	as.numeric(as.character(P8Result$Accuracy)),
+	as.numeric(as.character(P9Result$Accuracy)),
+	as.numeric(as.character(P10Result$Accuracy))),
+    P=c(rep(2,100),rep(3,100),rep(4,100),rep(5,100),rep(6,100),rep(7,100),rep(8,100),rep(9,100),rep(10,100)))
+repAcc$P <- as.factor(repAcc$P)
 ggplot(repAcc,aes(x=accuracy,colour=P))+geom_density(size=1.1,alpha=.8)+theme_classic()
-repSens = data.frame(sensitivity=c(P2Result$repStats[,2],P3Result$repStats[,2],P4Result$repStats[,2],P5Result$repStats[,2],P6Result$repStats[,2],P7Result$repStats[,2],P8Result$repStats[,2],P9Result$repStats[,2],P10Result$repStats[,2]),P=c(rep(2,100),rep(3,100),rep(4,100),rep(5,100),rep(6,100),rep(7,100),rep(8,100),rep(9,100),rep(10,100)))
-repSens$P = as.factor(repSens$P)
+repSens <- data.frame(sensitivity=c(as.numeric(as.character(P2Result$Sensitivity)),
+	as.numeric(as.character(P3Result$Sensitivity)),
+	as.numeric(as.character(P4Result$Sensitivity)),
+	as.numeric(as.character(P5Result$Sensitivity)),
+	as.numeric(as.character(P6Result$Sensitivity)),
+	as.numeric(as.character(P7Result$Sensitivity)),
+	as.numeric(as.character(P8Result$Sensitivity)),
+	as.numeric(as.character(P9Result$Sensitivity)),
+	as.numeric(as.character(P10Result$Sensitivity))),
+	P=c(rep(2,100),rep(3,100),rep(4,100),rep(5,100),rep(6,100),rep(7,100),rep(8,100),rep(9,100),rep(10,100)))
+repSens$P <- as.factor(repSens$P)
 ggplot(repSens,aes(x=sensitivity,colour=P))+geom_density(size=1.1,alpha=.8)+theme_classic()
-repSpec = data.frame(specificity=c(P2Result$repStats[,3],P3Result$repStats[,3],P4Result$repStats[,3],P5Result$repStats[,3],P6Result$repStats[,3],P7Result$repStats[,3],P8Result$repStats[,3],P9Result$repStats[,3],P10Result$repStats[,3]),P=c(rep(2,100),rep(3,100),rep(4,100),rep(5,100),rep(6,100),rep(7,100),rep(8,100),rep(9,100),rep(10,100)))
-repSpec$P = as.factor(repSpec$P)
+repSpec <- data.frame(specificity=c(as.numeric(as.character(P2Result$Specificity)),
+	as.numeric(as.character(P3Result$Specificity)),
+	as.numeric(as.character(P4Result$Specificity)),
+	as.numeric(as.character(P5Result$Specificity)),
+	as.numeric(as.character(P6Result$Specificity)),
+	as.numeric(as.character(P7Result$Specificity)),
+	as.numeric(as.character(P8Result$Specificity)),
+	as.numeric(as.character(P9Result$Specificity)),
+	as.numeric(as.character(P10Result$Specificity))),
+ 	P=c(rep(2,100),rep(3,100),rep(4,100),rep(5,100),rep(6,100),rep(7,100),rep(8,100),rep(9,100),rep(10,100)))
+repSpec$P <- as.factor(repSpec$P)
 ggplot(repSpec,aes(x=specificity,colour=P))+geom_density(size=1.1,alpha=.8)+theme_classic()
 
 # Find the informative set of genes
@@ -44,26 +72,25 @@ informativeSet <- read.table("~/Thesis/informativeSetDetermine.txt",sep='\t',hea
 informativeSet$T2 <- as.numeric(as.character(informativeSet$T2))
 # Find a candidate level for T2 to cutoff at. We'll go from anything not associated with T2 > 2.0 and increment cutoff each run.
 # Obtain accuracy estimates from modified bagging for genes not above each cutoff to help make decision.
-infSetTwo <- modifiedBagging(as.matrix(readyTrainingSet[,-which(colnames(readyTrainingSet) %in% unlist(informativeSet[informativeSet$T2>2.0,3:7]))]),classes,rep=100,stopP=5,stopT2=1000,proportion=.8)
+infSetTwo <- modifiedBagging(as.matrix(readyTrainingSet[,-which(colnames(readyTrainingSet) %in% unlist(informativeSet[informativeSet$T2>2.0,3:7]))]),classes,rep=100,stopP=5,stopT2=1000,proportion=.8,progressBar=T)
 write.table(infSetTwo$repStats,"~/Thesis/infSetTwo.txt",sep='\t',row.names=T,col.names=T)
-infSetThree <- modifiedBagging(as.matrix(readyTrainingSet[,-which(colnames(readyTrainingSet) %in% unlist(informativeSet[informativeSet$T2>3.0,3:7]))]),classes,rep=100,stopP=5,stopT2=1000,proportion=.8)
+infSetThree <- modifiedBagging(as.matrix(readyTrainingSet[,-which(colnames(readyTrainingSet) %in% unlist(informativeSet[informativeSet$T2>3.0,3:7]))]),classes,rep=100,stopP=5,stopT2=1000,proportion=.8,progressBar=T)
 write.table(infSetThree$repStats,"~/Thesis/infSetThree.txt",sep='\t',row.names=T,col.names=T)
-infSetFour <- modifiedBagging(as.matrix(readyTrainingSet[,-which(colnames(readyTrainingSet) %in% unlist(informativeSet[informativeSet$T2>4.0,3:7]))]),classes,rep=100,stopP=5,stopT2=1000,proportion=.8)
+infSetFour <- modifiedBagging(as.matrix(readyTrainingSet[,-which(colnames(readyTrainingSet) %in% unlist(informativeSet[informativeSet$T2>4.0,3:7]))]),classes,rep=100,stopP=5,stopT2=1000,proportion=.8,progressBar=T)
 write.table(infSetFour$repStats,"~/Thesis/infSetFour.txt",sep='\t',row.names=T,col.names=T)
-infSetFive <- modifiedBagging(as.matrix(readyTrainingSet[,-which(colnames(readyTrainingSet) %in% unlist(informativeSet[informativeSet$T2>5.0,3:7]))]),classes,rep=100,stopP=5,stopT2=1000,proportion=.8)
+infSetFive <- modifiedBagging(as.matrix(readyTrainingSet[,-which(colnames(readyTrainingSet) %in% unlist(informativeSet[informativeSet$T2>5.0,3:7]))]),classes,rep=100,stopP=5,stopT2=1000,proportion=.8,progressBar=T)
 write.table(infSetFive$repStats,"~/Thesis/infSetFive.txt",sep='\t',row.names=T,col.names=T)
 # Check out more granular cutoffs to see what may be appropriate.
-infSetThreePointFive <- modifiedBagging(as.matrix(readyTrainingSet[,-which(colnames(readyTrainingSet) %in% unlist(informativeSet[informativeSet$T2>3.5,3:7]))]),classes,rep=100,stopP=5,stopT2=1000,proportion=.8)
+infSetThreePointFive <- modifiedBagging(as.matrix(readyTrainingSet[,-which(colnames(readyTrainingSet) %in% unlist(informativeSet[informativeSet$T2>3.5,3:7]))]),classes,rep=100,stopP=5,stopT2=1000,proportion=.8,progressBar=T)
 write.table(infSetThreePointFive$repStats,"~/Thesis/infSetThreePointFive.txt",sep='\t',row.names=T,col.names=T)
-infSetTwoPointSFive <- modifiedBagging(as.matrix(readyTrainingSet[,-which(colnames(readyTrainingSet) %in% unlist(informativeSet[informativeSet$T2>2.75,3:7]))]),classes,rep=100,stopP=5,stopT2=1000,proportion=.8)
+infSetTwoPointSFive <- modifiedBagging(as.matrix(readyTrainingSet[,-which(colnames(readyTrainingSet) %in% unlist(informativeSet[informativeSet$T2>2.75,3:7]))]),classes,rep=100,stopP=5,stopT2=1000,proportion=.8,progressBar=T)
 write.table(infSetTwoPointSFive$repStats,"~/Thesis/infSetTwoPointSFive.txt",sep='\t',row.names=T,col.names=T)
-infSetTwoPointFive <- modifiedBagging(as.matrix(readyTrainingSet[,-which(colnames(readyTrainingSet) %in% unlist(informativeSet[informativeSet$T2>2.5,3:7]))]),classes,rep=100,stopP=5,stopT2=1000,proportion=.8)
+infSetTwoPointFive <- modifiedBagging(as.matrix(readyTrainingSet[,-which(colnames(readyTrainingSet) %in% unlist(informativeSet[informativeSet$T2>2.5,3:7]))]),classes,rep=100,stopP=5,stopT2=1000,proportion=.8,progressBar=T)
 write.table(infSetTwoPointFive$repStats,"~/Thesis/infSetTwoPointFive.txt",sep='\t',row.names=T,col.names=T)
 
 # Plot informative set, with line for our cutoff
 informativeSet$Index <- as.numeric(as.character(informativeSet$Index))
 ggplot(informativeSet,aes(x=Index,y=T2))+geom_point()+theme_bw()+geom_hline(yintercept=4.0,colour='red',alpha=.8)
-
 
 source('findInformativeBagging.R')
 # Get estimates for final informative set of genes now that cutoff decided (1000 modified bagging schema iterations). Store associated variables this time.
