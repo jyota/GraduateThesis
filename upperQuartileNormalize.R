f482e9f4b9da8dbdbdac7b6676206213c7765ec7
Jyota <jyotas@my.ccsu.edu>
2014-04-06 19:28:36 -0700

    add upper quartile scaling function

--- upperQuartileNormalize.R
+++ upperQuartileNormalize.R
@@ -11,6 +11,14 @@ upperQuartileNormalize <- function(data) {
     q3 <- apply(dataAdjusted, 2, quantile, probs = 0.75)
     d <- mean(q3)/q3
     dataNormalized <- t(t(data)*d)
-  
+  	cat('Data scaled using numerator: ', mean(q3), '\n')
   return(na.omit(dataNormalized))  
+}
+
+upperQuartileScale <- function(data, level){
+	# Scale more data to upper quartile = 'level', equivalent to what training data mean(upper quartile) was.
+	q3 <- apply(data, 2, quantile, probs = 0.75)
+	d <- level/q3
+	dataNormalized <- t(t(data)*d)
+	return(na.omit(dataNormalized))
 }
