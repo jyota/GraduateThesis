c31fae762eb3bd627cb2380c4b3023ef7352c1a0
ninja99 <ninja99@ninja99-OptiPlex-580.(none)>
2014-04-09 00:05:49 -0700

    updated test data rescaling routine

--- upperQuartileNormalize.R
+++ upperQuartileNormalize.R
@@ -17,7 +17,14 @@ upperQuartileNormalize <- function(data) {
 
 upperQuartileScale <- function(data, level){
 	# Scale more data to upper quartile = 'level', equivalent to what training data mean(upper quartile) was.
-	q3 <- apply(data, 2, quantile, probs = 0.75)
+	sumtot <- rowSums(data)
+    counts0 <- which(sumtot == 0)
+    if (length(counts0) > 0) {
+      dataAdjusted <- data[-counts0,]
+    } else {
+      dataAdjusted <- data
+    }
+    q3 <- apply(dataAdjusted, 2, quantile, probs = 0.75)
 	d <- level/q3
 	dataNormalized <- t(t(data)*d)
 	return(na.omit(dataNormalized))
