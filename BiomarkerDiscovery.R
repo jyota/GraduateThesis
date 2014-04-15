8e7d5d005c035d9b2545599c468b2020e0fc43f4
jyota <jyota@kogaRyu.(none)>
2014-04-14 18:22:41 -0700

    more updates

--- BiomarkerDiscovery.R
+++ BiomarkerDiscovery.R
@@ -117,7 +117,7 @@ write.table(fullSetFinal,"~/Thesis/fullSetFinal.txt",sep='\t',row.names=T,col.na
 
 # Get modified bagging estimates for 'non-informative' set of genes
 nonInfSetFinal <- findInformativeBagging(as.matrix(readyTrainingSet[,-which(colnames(readyTrainingSet) %in% unlist(informativeSet[informativeSet$T2>=3.0,3:10]))]),classes,rep=1000,stopP=8,stopT2=1000,proportion=.8,priors=c(.5,.5))
-write.table(nonInfSetFinal$repStats,"~/Thesis/nonInfSetFinal.txt",sep='\t',row.names=T,col.names=T)
+write.table(nonInfSetFinal,"~/Thesis/nonInfSetFinal.txt",sep='\t',row.names=T,col.names=T)
 
 # Cluster by Pearson's correlation distance 
 require(Hmisc)
